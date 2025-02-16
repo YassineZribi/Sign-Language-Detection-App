26c0fbae5438b2a82557fa6bbaaa4728f1447edf
Yassine Zribi <yassinezribiyassine@gmail.com>
2025-02-16 19:27:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Project Setup
+
+- Anaconda should be installed
+
+cd Projects/
+git clone repoository-url
+cd repository-folder/
+conda create -n sign python=3.8 -y
+conda activate sign
+pip install -r requirements.txt 
+python app.py
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '1.0.0',
+    author= 'yassine',
+    author_email= 'yassinezribiyassine@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
